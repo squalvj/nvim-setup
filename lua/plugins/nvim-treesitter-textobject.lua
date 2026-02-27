4ec398f013a5133c8006ea5d73e2832d88a3866b
squalvj <squalvj@gmail.com>
2026-02-27 07:54:19 +0700

    chore: add indentwise

---
+++ lua/plugins/nvim-treesitter-textobject.lua
@@ -0,0 +1,51 @@
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
+  init = function()
+    vim.g.no_plugin_maps = true
+  end,
+  config = function()
+    require("nvim-treesitter-textobjects").setup({
+      select = { lookahead = true },
+    })
+
+    -- SELECT (x=visual, o=operator-pending)
+    vim.keymap.set({ "x", "o" }, "am", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "im", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ac", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ic", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "as", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+    end)
+
+    -- SWAP
+    vim.keymap.set("n", "<leader>a", function()
+      require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+    end)
+    vim.keymap.set("n", "<leader>A", function()
+      require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
+    end)
+
+    -- MOVE (n/x/o)
+    vim.keymap.set({ "n", "x", "o" }, "]m", function()
+      require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "[m", function()
+      require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "]]", function()
+      require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "[[", function()
+      require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+    end)
+  end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,4 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   lazy = false,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
 }
